a531c33a245ad3142f7290efc3534a05eff05bae
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-05-01 15:45:44 +0300

    Created Animal class

---
+++ src/ua/edu/cbs/lms/hometask_adv_5/task4/Animal.java
@@ -0,0 +1,13 @@
+package ua.edu.cbs.lms.hometask_adv_5.task4;
+
+public class Animal {
+    private String name;
+    protected int age;
+    public boolean isPredator;
+
+    public Animal(String name, int age, boolean isPredator) {
+        this.name = name;
+        this.age = age;
+        this.isPredator = isPredator;
+    }
+}
